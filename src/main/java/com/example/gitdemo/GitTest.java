83cd728740587972aa9c0eb5c7c953d8783e2fd8
duola <1595899107@qq.com>
2022-05-09 11:28:35 +0800

    git test second

--- src/main/java/com/example/gitdemo/GitTest.java
+++ src/main/java/com/example/gitdemo/GitTest.java
@@ -7,5 +7,7 @@ package com.example.gitdemo;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git! second");
+        System.out.println("hello git! third");
     }
 }
