f787157033f8f4f142b836ab3c124cbb0f6b04d3
duola <1595899107@qq.com>
2022-05-09 11:44:24 +0800

    git test hot-fix second

--- src/main/java/com/example/gitdemo/GitTest.java
+++ src/main/java/com/example/gitdemo/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git! second");
         System.out.println("hello git! third");
+        System.out.println("hello git! hot-fix");
     }
 }
